b4636d9872ec3b1c3e82087cc63dfb8ecd29e07a
20chan <2@0chan.dev>
2020-10-13 00:17:11 +0900

    rss feed validate with joi

--- src/feeds/feed.ts
+++ src/feeds/feed.ts
@@ -1,15 +1,31 @@
-interface IFeedChannel {
+import Joi from "joi";
+
+export interface IFeedChannel {
     title: string;
     description: string;
     link: string;
     items: IFeedItem[];
 }
 
-interface IFeedItem {
+export interface IFeedItem {
     title: string;
     description: string;
     link: string;
     guid: string;
 }
 
-type IFeedImpl = (src: string) => Promise<IFeedChannel>;
+export const schema = Joi.object({
+    title: Joi.string().required(),
+    description: Joi.string().required(),
+    link: Joi.string().uri().required(),
+    items: Joi.array().items(
+        Joi.object({
+            title: Joi.string().required(),
+            description: Joi.string().required(),
+            link: Joi.string().uri().required(),
+            guid: Joi.string().required(),
+        })
+    ).required(),
+});
+
+export type IFeedImpl = (src: string) => Promise<IFeedChannel>;
--- src/feeds/impls/rss.ts
+++ src/feeds/impls/rss.ts
@@ -1,9 +1,27 @@
 import fetch from "node-fetch";
-import { parseStringPromise } from "xml2js";
+import { parse } from "fast-xml-parser";
+import { IFeedImpl, schema } from "../feed";
 
 export const getFeeds: IFeedImpl = async (src) => {
     const resp = await fetch(src);
     const body = await resp.text();
-    const data = await parseStringPromise(body);
-    return data.rss.channel[0];
+    const data = parse(body);
+    const channelRaw = data.rss.channel;
+    const items = channelRaw.item.map((m: any) => ({
+        title: m.title,
+        description: m.description,
+        link: m.link,
+        guid: m.guid,
+    }));
+    const result = {
+        title: channelRaw.title,
+        description: channelRaw.description,
+        link: channelRaw.link,
+        items,
+    };
+
+    try {
+        await schema.validateAsync(result);
+        return result;
+    } catch (err) { throw err; }
 };
