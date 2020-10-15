9c9bb3266439ead85d229cb9016a9f16d8159acb
20chan <2@0chan.dev>
2020-10-15 14:24:09 +0900

    use rss-parser instead of fast-xml-parser

--- src/feeds/impls/rss.ts
+++ src/feeds/impls/rss.ts
@@ -1,23 +1,21 @@
-import fetch from "node-fetch";
-import { parse } from "fast-xml-parser";
-import { IFeedImpl, schema } from "../feed";
+import Parser from "rss-parser";
+import { IFeedChannel, IFeedImpl, schema } from "../feed";
+
+const parser = new Parser();
 
 export const getFeeds: IFeedImpl = async (src) => {
     try {
-        const resp = await fetch(src);
-        const body = await resp.text();
-        const data = parse(body);
-        const channelRaw = data.rss.channel;
-        const items = channelRaw.item.map((m: any) => ({
-            title: m.title,
-            description: m.description,
-            link: m.link,
-            guid: m.guid,
+        const feed = await parser.parseURL(src);
+        const items = feed.items === undefined ? [] : feed.items.map(m => ({
+            title: m.title || "",
+            description: m.description || "",
+            link: m.link || "",
+            guid: m.guid || "",
         }));
         const result = {
-            title: channelRaw.title,
-            description: channelRaw.description,
-            link: channelRaw.link,
+            title: feed.title || "",
+            description: feed.description || "",
+            link: feed.link || "",
             items,
         };
         await schema.validateAsync(result);
